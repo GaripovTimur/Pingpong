aab8838c33ddd8a9cd08e1c950649ef9c397daf5
Artem <denispro123@yandex.ru>
2022-04-03 18:44:18 +0300

    Добавил движене мяча

--- pinpongg.py
+++ pinpongg.py
@@ -30,17 +30,16 @@ class Player(GameSprite):
             self.rect.x -= 10
         if keys_pressed[K_RIGHT] and  self.rect.x < 630:
             self.rect.x += 10
-class Ball(GameSprite):
-    pass
+
 font.init()
 font = font.SysFont('Arial',27)
-sprite1 = Player(('bullet.png'),25,25,10)
-sprite2 = Player(('bullet.png'),430,400,10)
-sprite3 = Ball(('asteroid.png'),100,400,10)
-win = font.render('YOU WIN!', True, (0,250,0))
-lose = font.render('YOU LOSE!', True, (225,0,0))
+sprite1 = Player(('bed.png'),25,25,10)
+sprite2 = Player(('bed.png'),430,400,10)
+sprite3 = GameSprite(('asteroid.png'),300,200,10)
+lose = font.render('LOSE!', True, (225,0,0))
 run = True
 finish = False
+score = 0
 while run:
     window.blit(background,(0,0))
     sprite1.update()
@@ -49,23 +48,29 @@ while run:
     sprite2.reset()
     sprite3.update()
     sprite3.reset()
+    txt = font.render('Число отбиваний:' + str(score),1,(225,0,0))
+    window.blit(txt,(0,30))
+    if finish != True:
+        sprite3.rect.x += speed_x
+        sprite3.rect.y += speed_y
+
     if sprite.collide_rect(sprite1,sprite3) or sprite.collide_rect(sprite2,sprite3):
         speed_y *= -1
+        score += 1 
+
     if sprite3.rect.x <= 0:
         speed_x *= -1
-    if sprite3.rect.x <= 630:
+
+    if sprite3.rect.x >= 630:
         speed_x *= -1
 
+    if sprite3.rect.y <= 0:
+        window.blit(lose, (300,200))
+        finish = True
 
-
-
-
-
-
-
-
-
-
+    if sprite3.rect.y >= 450:
+            window.blit(lose, (300,200))
+            finish = True        
 
     for e in event.get():
         if e.type == QUIT:
